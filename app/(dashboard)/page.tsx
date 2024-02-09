383c7df900508dd376672ffec08dddf858828fd5
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-02-09 15:47:15 -0600

    Empty States

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,6 +2,7 @@
 
 import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
 
 interface DashboardPageProps {
   searchParams: {
@@ -15,8 +16,11 @@ const DashboardPage = ({ searchParams }: DashboardPageProps) => {
 
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
-      {JSON.stringify(searchParams)}
-      {!organization ? <EmptyOrg /> : <p>Board List</p>}
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </div>
   );
 };
